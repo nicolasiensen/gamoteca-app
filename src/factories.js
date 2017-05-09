180c09965628b3e5e8da3df239df3d0c99e47946
Nícolas Iensen <nicolas.iensen@gmail.com>
2017-05-09 14:03:08 +0200

    Update game factory to have unique uuid's

--- src/factories.js
+++ src/factories.js
@@ -1,9 +1,13 @@
+let gameCount = 0;
+
 export function createGame(attributes) {
+  gameCount += 1;
+  
   return {
     id: 1505,
     image_url: "https://apollo2.dl.playstation.net/cdn/EP2002/CUSA...",
     name: "Rocket League®",
-    uuid: "EP2002-CUSA01433_00-ROCKETLEAGUEEU01",
+    uuid: `EP2002-CUSA01433_00-ROCKETLEAGUEEU0${gameCount}`,
     platforms: ["PS4™"],
     released_at: "2015-07-07 00:00:00",
     price: 1399,
